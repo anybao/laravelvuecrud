da3dcfe96a12c95097e2b9961114b77113b4c32e
Quang Huy <anybao4@gmail.com>
2018-05-09 07:38:52 +0700

    INit Laravel 5.5

---
+++ resources/assets/js/components/Articles.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Articles</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                articles: [],
+                article: {
+                    id:'',
+                    title: '',
+                    body: '',
+                },
+                article_id:''
+            }
+        }
+    }
+</script>
